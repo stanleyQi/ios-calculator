39e0d9c74011532a6730efe21d76788cc2d00a48
qili <qili811227@gmail.com>
2019-09-22 19:50:12 +1200

    initial commit.

---
+++ calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calculator
+//
+//  Created by liqi on 22/09/19.
+//  Copyright © 2019 liqi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
